4168a3d976e628a240752b68b5d36ba94654cb95
Omar <omarisel01@gmail.com>
2013-05-12 16:12:53 -0500

    suma todos los numeros de la misma linea

--- suma/src/suma/Suma.java
+++ suma/src/suma/Suma.java
@@ -1,10 +1,25 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+Limite de tiempo : 0.5 seg.   Total runs : 1825  Aceptados : 607
+
+Sumar a y b;La entrada y salida es mediante la lectura y escritura de archivos. 
+Entrada (data.in):
+Dos enteros separados por un espacio.
+
+Salida (data.out):
+Un entero que sea la suma de a y b.
+
+Ejemplo:
+
+Entrada:
+10 10
+12 12
+
+Salida:
+20
+24
  */
 package suma;
 import java.io.*;
-import java.lang.StringBuilder;
 import java.util.ArrayList;
 /**
  *
@@ -17,9 +32,9 @@ public class Suma {
     
       public void readFile(){
      
-            String data = "";
+        String data = "";
         String[] retriveData = {};
-        ArrayList collectionData = new ArrayList();
+        ArrayList<Integer> collectionData = new ArrayList<Integer>();
  
         try {
             
@@ -28,29 +43,32 @@ public class Suma {
 // leer archivo para obtener el valor entero
 
             BufferedReader bf = new BufferedReader(new FileReader(FILE_NAME_INPUT));
-
-
-            while ((data = bf.readLine()) != null) {
-
-
+            
+            int  counter;
+            int _number;
+            while ((data = bf.readLine()) != null) {//leemos una linea readLine del data.in
+                
                 retriveData = data.split(" ");
-
-                for (String numer : retriveData) {
-
-                    collectionData.add(numer);
+                counter = 0;
+                
+                for (String number : retriveData) {
+                    
+                    //cast a Entero
+                    _number = Integer.parseInt(number);
+                    
+                    // sumamos todos los numeros de la misma fila
+                    counter += _number; 
+     
                 }
-
-
+   
+                collectionData.add(counter);
+ 
             }
  
-            for(int index = 0 ; index  < collectionData.size() ; index ++){
-
-                System.out.print("data "+collectionData.get(index));
-                
-                }
-
+           
+             System.out.print( collectionData.get(0)+"\n"+collectionData.get(1));
             
-
+ 
         } catch (Exception e) {
             
 // cachamos el error
